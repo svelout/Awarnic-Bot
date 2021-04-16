37f4fa88100ae718717a411ac6efaf685f3b2724
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-04-16 22:54:27 +0300

    s

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ colours = {
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"на участников"))
+    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f"на участников"))
     print("Бот начал работать")
 
 @bot.command(name='test')
