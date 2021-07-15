2a3915c2dfe92a2756f16893c76dda55ee17649b
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-07-15 13:14:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,10 +32,11 @@ async def on_ready():
     guild = discord.Guild
     servers = len(bot.guilds)
     members = 0
-    for guild in bot.guilds:
-        members += guild.member_count - 1
+    while:
+     for guild in bot.guilds:
+         members += guild.member_count - 1
 
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=str(f"на {members} участников")))
+      await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=str(f"на {members} участников")))
     print("Бот начал работать")
 
 @bot.event
